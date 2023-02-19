66d830be34dfe4f0d72407da4aac9369b2bddeca
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-02-20 01:50:36 +0600

    Updated

---
+++ js/todo.js
@@ -0,0 +1,27 @@
+let count =0;
+document.getElementById('add').addEventListener('click',function(){
+    const inputField = document.getElementById('input').value;
+    count++;
+    //rows-element
+    // Parent node table row;
+    const rowsElement = document.getElementById('rows-element');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${count}</td>
+    <td>${inputField}</td>
+    <td>
+    <button class="delete">Delete</button>
+    </td>
+    `;
+     rowsElement.appendChild(tr);
+
+    //  For deleteing:;
+
+    const deleteBtn = document.querySelectorAll(".delete");
+    for(const value of deleteBtn){
+        value.addEventListener("click",function(event){
+           const myitem = event.target.parentNode.parentNode;
+           myitem.innerHTML = '';
+        })
+    }
+})
